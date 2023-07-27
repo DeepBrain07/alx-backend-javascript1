7e8d9c412d1f25d31573b37ec2a91006f40ebf52
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-07-27 02:27:22 +0100

    Task 2 completed

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+    promise
+        .then(function() {
+            return ({status: 200, body: "Success"});
+        })
+        .catch(function() {
+            return Error
+        })
+        .finally(function() {
+            console.log("Got a response from the API");
+        })
+}
