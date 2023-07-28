d0cef32e9cf88fba74194aab1665fcaf4b5d4bce
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-07-28 05:23:54 +0100

    Debugging

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,11 +1,5 @@
 export default class ClassRoom {
     constructor(maxStudentsSize) {
-        if (Number.isInteger(maxStudentsSize)) {
-            this.__maxStudentsSize = maxStudentsSize;
-        }
-    }
-    //getter
-    get _maxStudentsSize() {
-        return this.__maxStudentsSize
+        this._maxStudentsSize = maxStudentsSize;
     }
 }
